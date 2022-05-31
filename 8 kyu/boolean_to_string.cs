5a0354f2771e96b5ce8da787b9bfe89e324d5e54
amos408 <106403895+amos408@users.noreply.github.com>
2022-05-31 22:17:11 +0300

    Create boolean_to_string.cs

---
+++ 8 kyu/boolean_to_string.cs
@@ -0,0 +1,17 @@
+//Convert a Boolean to a String
+//Implement a function which convert the given boolean value into its string representation.
+
+//Note: Only valid inputs will be given.
+
+
+
+using System;
+
+public class kata
+{
+  public static string boolean_to_string(bool b)
+  {
+  //Please don't delete me!
+    return b.ToString();
+  }
+}
