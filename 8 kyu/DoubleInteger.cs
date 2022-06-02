e1245a92baa362077a74999a6ab579d604b512f1
amos408 <106403895+amos408@users.noreply.github.com>
2022-06-02 15:30:00 +0300

    Create DoubleInteger.cs

---
+++ 8 kyu/DoubleInteger.cs
@@ -0,0 +1,12 @@
+//You Can't Code Under Pressure #1
+//Code as fast as you can! You need to double the integer and return it.
+using System;
+
+public static class Kata
+{
+    public static int DoubleInteger(int n)
+    {
+        //Double the input!
+        return n * 2;
+    }
+}
