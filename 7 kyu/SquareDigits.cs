1d6035890c12a35b537337f5836fae9cea2ca6fa
amos408 <106403895+amos408@users.noreply.github.com>
2022-06-08 10:48:22 +0300

    Create SquareDigits.cs

---
+++ 7 kyu/SquareDigits.cs
@@ -0,0 +1,28 @@
+//Square Every Digit
+//Welcome.In this kata, you are asked to square every digit of a number and concatenate them.
+
+//For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+//Note: The function accepts an integer and returns an integer
+using System;
+
+public class Kata
+{
+    public static int SquareDigits(int n)
+    {
+        int places = 0;
+        int result = 0;
+
+        while (n > 0)
+        {
+
+            int digit = n % 10;
+            int square = digit * digit;
+            result += (int)Math.Pow(10, places) * square;
+            places += square > 10 ? 2 : 1;
+            n /= 10;
+
+        }
+        return result;
+    }
+}
