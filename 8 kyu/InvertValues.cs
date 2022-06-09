93498cde480e5ba408e3c90665de678e2c89a03b
amos408 <106403895+amos408@users.noreply.github.com>
2022-06-09 21:46:53 +0300

    Create InvertValues.cs

---
+++ 8 kyu/InvertValues.cs
@@ -0,0 +1,21 @@
+//Invert values
+//Given a set of numbers, return the additive inverse of each.
+//Each positive becomes negatives, and the negatives become positives.
+
+//invert([1, 2, 3, 4, 5]) == [-1, -2, -3, -4, -5]
+//invert([1, -2, 3, -4, 5]) == [-1, 2, -3, 4, -5]
+//invert([]) == []
+
+using System.Linq;
+namespace Solution
+{
+    public static class ArraysInversion
+    {
+        public static int[] InvertValues(int[] input)
+        {
+            //Code it!
+            return input.Select(x => -x).ToArray();
+        }
+    }
+}
+
