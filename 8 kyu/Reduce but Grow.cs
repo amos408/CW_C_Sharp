6ec7f30a96a58ca777776c6d259772939fadd011
amos408 <106403895+amos408@users.noreply.github.com>
2022-06-19 21:33:29 +0300

    Create Reduce but Grow.cs

---
+++ 8 kyu/Reduce but Grow.cs
@@ -0,0 +1,15 @@
+//Beginner - Reduce but Grow
+//Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+//[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+public class Kata
+{
+  public static int Grow(int[] x)
+  {
+    var result = 1;
+    for(var i = 0; i < x.Length; i++)
+      result *= x[i];
+    return result;
+  }
+}
