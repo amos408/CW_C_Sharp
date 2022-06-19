4ff9c199d1eae94a61ddf0fe0fe0015e14ed0a14
amos408 <106403895+amos408@users.noreply.github.com>
2022-06-19 21:43:53 +0300

    Create multiplying two numbers.cs

---
+++ 8 kyu/multiplying two numbers.cs
@@ -0,0 +1,16 @@
+//Function 3 - multiplying two numbers
+//Implement a function which multiplies two numbers.
+
+// Make a function with the class name Kata and the function name Multiply that takes in two
+// integers and multiplies them
+
+  using System;
+
+public class Kata
+{
+    public static int Multiply(int a, int b) 
+    {
+        return a * b;
+    }
+}
+  
