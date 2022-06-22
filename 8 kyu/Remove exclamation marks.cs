c3a9272a56459a1fb7cc619f7d9729ac2d346e9b
amos408 <106403895+amos408@users.noreply.github.com>
2022-06-22 21:53:13 +0300

    Create Remove exclamation marks.cs

---
+++ 8 kyu/Remove exclamation marks.cs
@@ -0,0 +1,10 @@
+//Remove exclamation marks
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+public class Kata
+{
+  public static string RemoveExclamationMarks(string s)
+  {
+    // Your code goes here
+    return s.Replace("!", "");
+  }
+}
