24a790639a01dd9da6399382df9cb490183a8e81
amos408 <106403895+amos408@users.noreply.github.com>
2022-06-26 14:46:05 +0300

    Create MakeUpperCase.cs

---
+++ 8 kyu/MakeUpperCase.cs
@@ -0,0 +1,11 @@
+//MakeUpperCase
+//Write a function which converts the input string to uppercase.
+  public class Kata
+  {
+        public static string MakeUpperCase(string str)
+        {
+             //return "str to upperCase";
+          return str.ToUpper();
+        }
+
+  }
