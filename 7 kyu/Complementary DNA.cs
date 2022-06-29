c7f0b239de11e94204bad633bb468efb6ea7070d
amos408 <106403895+amos408@users.noreply.github.com>
2022-06-29 08:57:48 +0300

    Create Complementary DNA.cs

---
+++ 7 kyu/Complementary DNA.cs
@@ -0,0 +1,48 @@
+//Complementary DNA
+//Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+//If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+//In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+//More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+//Example: (input --> output)
+
+//"ATTGC" --> "TAACG"
+//"GTAT" --> "CATA"
+
+public class DnaStrand 
+    {
+        public static string MakeComplement(string dna)
+        {
+            //Your code
+            string complement = "";
+
+        foreach (char symbol in dna)
+        {
+            if (symbol == 'A')
+            {
+                complement += 'T';
+            }
+            else if (symbol == 'T')
+            {
+                complement += 'A';
+            }
+            else if (symbol == 'C')
+            {
+                complement += 'G';
+            }
+            else if (symbol == 'G')
+            {
+                complement += 'C';
+            }
+            else
+            {
+                complement += symbol;
+            }
+        }
+
+        return complement;
+        }
+    }
