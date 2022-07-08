9bde4b6013b6da9695d2f8442e0f93232998f4cb
amos408 <106403895+amos408@users.noreply.github.com>
2022-07-08 20:59:20 +0300

    Create Super Duper Easy.cs

---
+++ 8 kyu/Super Duper Easy.cs
@@ -0,0 +1,15 @@
+//Super Duper Easy
+//Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+//Note: in C#, you'll always get the input as a string, so the above applies if the string isn't representing a double value.
+using System;
+public class Kata
+{
+  public static string Problem(String a)
+  {
+    //magic code 
+    if (double.TryParse(a, out var number))
+                return number * 50 + 6 + "";
+            return "Error";
+  }
+}
