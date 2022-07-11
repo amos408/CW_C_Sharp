ee7660b6a9fcfab43642d152aca9c32d32e7250a
amos408 <106403895+amos408@users.noreply.github.com>
2022-07-11 21:51:02 +0300

    Create Sort Numbers.cs

---
+++ 7 kyu/Sort Numbers.cs
@@ -0,0 +1,16 @@
+//Sort Numbers
+//Finish the solution so that it sorts the passed in array of numbers. 
+//If the function passes in an empty array or null/nil value then it should return an empty array.
+
+//For example:
+
+//SortNumbers(new int[] { 1, 2, 10, 50, 5 }); // should return new int[] { 1, 2, 5, 10, 50 }
+//SortNumbers(null); // should return new int[] { }
+
+using System;
+using System.Linq;
+public class Kata
+{
+   public static int[] SortNumbers(int[] nums) =>
+        nums?.OrderBy(n => n).ToArray() ?? Array.Empty<int>();
+}
