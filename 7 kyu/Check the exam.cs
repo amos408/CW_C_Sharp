1bcb729d001debfadf49fb9e01c015b673ba9dbe
amos408 <106403895+amos408@users.noreply.github.com>
2022-07-14 07:59:48 +0300

    Create Check the exam.cs

---
+++ 7 kyu/Check the exam.cs
@@ -0,0 +1,22 @@
+//Check the exam
+//The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+//The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+//If the score < 0, return 0.
+
+//For example:
+
+//checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+//checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+//checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+//checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+using System;
+using System.Linq;
+public static class Kata
+{
+    public static int CheckExam(string[] arr1, string[] arr2)
+    {
+        return Math.Max(arr2.Select((t, r) => t == "" ? 0 : t == arr1[r] ? 4 : -1).Sum(), 0);
+    }
+}
