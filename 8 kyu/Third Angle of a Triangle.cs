bd618f7f1d381aaea9a64e642eef589c1e2bea5d
amos408 <106403895+amos408@users.noreply.github.com>
2022-07-15 08:11:22 +0300

    Create Third Angle of a Triangle.cs

---
+++ 8 kyu/Third Angle of a Triangle.cs
@@ -0,0 +1,16 @@
+//Third Angle of a Triangle
+//You are given two interior angles (in degrees) of a triangle.
+
+//Write a function to return the 3rd.
+
+//Note: only positive integers will be tested.
+
+using System;
+
+public static class Kata
+{
+  public static int OtherAngle(int a, int b)
+  {
+    return 180 - (a + b);
+  }
+}
