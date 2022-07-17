d98b581f3a5270eca0b08dd198440a0b56386683
amos408 <106403895+amos408@users.noreply.github.com>
2022-07-17 08:07:50 +0300

    Create Summing a number's digits.cs

---
+++ 7 kyu/Summing a number's digits.cs
@@ -0,0 +1,17 @@
+//Summing a number's digits
+//Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+//For example: (Input --> Output)
+
+//10 --> 1
+//99 --> 18
+//-32 --> 5
+
+//Let's assume that all numbers in the input will be integer values.
+
+using System;
+using System.Linq;
+public class Kata
+{
+   public static int SumDigits(int number) => (int)(Math.Abs(number) + "").Select(char.GetNumericValue).Sum();  
+}
