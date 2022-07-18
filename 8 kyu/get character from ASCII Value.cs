50d247f915816b4d611f30ea1c248793857b49ad
amos408 <106403895+amos408@users.noreply.github.com>
2022-07-18 08:28:42 +0300

    Create get character from ASCII Value.cs

---
+++ 8 kyu/get character from ASCII Value.cs
@@ -0,0 +1,19 @@
+//get character from ASCII Value
+//Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+//Example:
+
+//get_char(65)
+
+//should return:
+
+'A'
+
+//For ASCII table, you can refer to http://www.asciitable.com/
+public class Kata
+{
+  public static char GetChar(int charcode) => (char)charcode;
+  //{
+    // ...
+  //}
+}
