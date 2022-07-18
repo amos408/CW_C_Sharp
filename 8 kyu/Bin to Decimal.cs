9905b0c4888263f2f5b6389dcfeb4834423b6b42
amos408 <106403895+amos408@users.noreply.github.com>
2022-07-18 20:18:55 +0300

    Create Bin to Decimal.cs

---
+++ 8 kyu/Bin to Decimal.cs
@@ -0,0 +1,15 @@
+//Bin to Decimal
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+using System;
+
+namespace Solution
+{
+  public static class Program
+  {
+    public static int binToDec(string s)
+    {
+      if(string.IsNullOrWhiteSpace(s)) return 0;
+  	return Convert.ToInt32(s, 2);
+    }
+  }
+}
