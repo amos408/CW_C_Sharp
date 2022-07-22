bf4444f86f3d5fcf5e8e721afcf3150490c0091c
amos408 <106403895+amos408@users.noreply.github.com>
2022-07-22 08:30:33 +0300

    Create Filling an array (part 1).cs

---
+++ 8 kyu/Filling an array (part 1).cs
@@ -0,0 +1,20 @@
+//Filling an array (part 1)
+//We want an array, but not just any old array, an array with contents!
+
+//Write a function that produces an array with the numbers 0 to N-1 in it.
+
+//For example, the following code will result in an array containing the numbers 0 to 4:
+
+//arr(5) // => [0,1,2,3,4]
+
+//Note: The parameter is optional. So you have to give it a default value.
+
+using System.Collections.Generic;
+using System.Linq;
+public class Kata
+{
+  public static int[] Arr(int N)
+  {
+    return Enumerable.Range(0, N).ToArray();
+  }
+}
