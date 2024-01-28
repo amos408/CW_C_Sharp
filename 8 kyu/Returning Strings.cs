b013fc9e0eefa7b381c0f654289e8deaf93f1722
amos408 <106403895+amos408@users.noreply.github.com>
2024-01-28 18:35:31 +0200

    Create Returning Strings.cs

---
+++ 8 kyu/Returning Strings.cs
@@ -0,0 +1,15 @@
+//Returning Strings
+//Make a function that will return a greeting statement that uses an input; your program should return, 
+//"Hello, <name> how are you doing today?".
+
+//[Make sure you type the exact thing I wrote or the program may not execute properly]
+using System;
+
+public static class Kata
+{
+  public static string Greet(string name)
+  {
+   // throw new NotImplementedException("Greet is not implemented.");
+    return $"Hello, {name} how are you doing today?";
+  }
+}
