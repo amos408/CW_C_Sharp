26624ff39bc903f373c00c8786ead5421808f017
amos408 <106403895+amos408@users.noreply.github.com>
2024-01-28 18:19:07 +0200

    Create  Function 2 - squaring an argument.cs

---
+++ 8 kyu/Function 2 - squaring an argument.cs
@@ -0,0 +1,9 @@
+//Function 2 - squaring an argument
+//Now you have to write a function that takes an argument and returns the square of it.
+public class Kata
+{
+  public static double Square(double a) => a * a;
+
+  // Write a static function "Square" here which takes a double
+  // and returns the square of that double
+}
